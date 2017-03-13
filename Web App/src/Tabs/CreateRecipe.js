62347b9a11920e46c6e0d0ffbe0f75ddc236bda2
Kadhir Manickam <kadhirvelm@berkeley.edu>
2017-03-13 12:35:38 -0700

    Most everything working

--- Web App/src/Tabs/CreateRecipe.js
+++ Web App/src/Tabs/CreateRecipe.js
@@ -3,18 +3,31 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
-import Flexbox from 'flexbox-react'
+import Upload from 'material-ui-upload/Upload'
 
 const validate = values => {
 	const errors = {}
-	console.log(values)
-	const requiredFields = [ 'recipeText', 'Image', 'Ingredients', 'Directions' ]
+	const requiredFields = [ 'recipeNameText', 'recipeImage', 'ingredientsList', 'directions' ]
 	requiredFields.forEach( (field) => {
 		if (!values[field]) {
 			errors[ field ] = 'Required'
 		}
-		return errors
+		if(values.recipeImage && !/image\/.*/.test(values.recipeImage[0].type)) {
+			errors.recipeImage = 'Only images are allowed'
+		}
 	})
+	return errors
+}
+
+const picturePreview = {
+	marginTop: '10px',
+	height: 'auto',
+	width: 'auto',
+	maxWidth: '150px',
+	maxHeight: '150px',
+	display: 'block',
+	marginLeft: 'auto',
+	marginRight: 'auto',
 }
 
 class CreateRecipe extends React.Component {
@@ -28,12 +41,16 @@ class CreateRecipe extends React.Component {
 		})
 	}
 
+	componentWillMount(){
+		this.props.reset()
+	}
+
 	componentWillReceiveProps(nextProps){
 		this.setState(this.propsConst(nextProps))
 	}
 
 	recipeNameText = ({ input, meta: { touched, error } }) => (
-		<TextField id='recipeText'
+		<TextField id='recipeNameText'
 		floatingLabelText='Recipe Name'
 		errorText={ touched && error }
 		fullWidth={ true }
@@ -41,20 +58,46 @@ class CreateRecipe extends React.Component {
 		/>
 	)
 
+	onFileLoad = (e) => this.setState({ picture: e.target.result })
+
 	imageField = ({ input, meta: {touched, error } }) => (
-		<TextField id='recipeImage'
-		floatingLabelText='Recipe Image'
+		<div>
+			<Upload id='recipeImage' label='Upload Image' onFileLoad={ this.onFileLoad } style={ { width: '100%' } } { ...input } />
+			<font color='red'> { touched && error } </font>
+		</div>
+	)
+
+	ingredientsList = ({ input, meta: {touched, error } }) => (
+		<TextField id='ingredientsList'
+		floatingLabelText='Ingredients'
 		errorText={ touched && error }
+		multiLine={ true } rows={ 3 } rowsMax={ 5 } 
 		fullWidth={ true }
 		{ ...input }
-		)
+		/>
+	)
+
+	directions = ({ input, meta: {touched, error } }) => (
+		<TextField id='directions'
+		floatingLabelText='Directions'
+		errorText={ touched && error }
+		multiLine={ true } rows={ 5 } rowsMax={ 7 }
+		fullWidth={ true }
+		{ ...input }
+		/>
+	)
 
 	render(){
 		const { handleSubmit } = this.props
 		return (
 			<form onSubmit={ handleSubmit }>
-				<Field name='recipeText' component={ this.recipeNameText } label='Recipe Name' />
+				<Field name='recipeNameText' component={ this.recipeNameText } label='Recipe Name' />
 				<Field name='recipeImage' component={ this.imageField } label='Recipe Image' />
+				{ this.state.picture &&
+					<img src={ this.state.picture } style={ picturePreview } role='presentation' />
+				}
+				<Field name='ingredientsList' component={ this.ingredientsList } label='Ingredient List' />
+				<Field name='directions' component={ this.directions } label='Directions' />
 				<RaisedButton id='submit'
 	        type='submit'
 	        label='Create Recipe'
